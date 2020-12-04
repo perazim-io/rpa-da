535b601c1aa3870c36f318c3521567652e9e2b51
shajeelawrence <shajeelawrence2709@gmail.com>
2020-12-04 18:51:27 +0530

    Initial Commit

---
+++ src/main/java/io/perazim/rpada/RpaDaApplication.java
@@ -0,0 +1,13 @@
+package io.perazim.rpada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpaDaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpaDaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/perazim/rpada/RpaDaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.perazim.rpada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpaDaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
